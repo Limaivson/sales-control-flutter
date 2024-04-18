9b96ff68c46fb2e6640a010724451da9bfc1de3e
Ivson De Lima Santos <124381236+Limaivson@users.noreply.github.com>
2024-04-18 19:45:10 -0300

    feat-fiados-class

---
+++ sales_control/lib/fiados.dart
@@ -0,0 +1,8 @@
+class Fiado {
+  final String cliente;
+  final String endereco;
+  final String funcionario;
+  final String pagamento;
+
+  Fiado({required this.cliente, required this.endereco, required this.funcionario, required this.pagamento});
+}
