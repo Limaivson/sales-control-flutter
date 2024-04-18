d11b228a424065df0ce5d37fdb3fbb5a408132a0
Ivson De Lima Santos <124381236+Limaivson@users.noreply.github.com>
2024-04-18 19:44:54 -0300

    feat-main

--- sales_control/lib/main.dart
+++ sales_control/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:sales_control/page/pageFiados.dart';
+import 'package:sales_control/page/pageRecebidos.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
@@ -24,38 +27,29 @@ class HomeScreen extends StatelessWidget {
         title: const Text('Menu Principal'),
       ),
       body: Center(
-        child: Column(
+        child: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const FiadosScreen()),
-                );
-              },
-              child: const Text('Fiados'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const RecebidoScreen()),
-                );
-              },
-              child: const Text('Recebido'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const EditarScreen()),
-                );
-              },
-              child: const Text('Editar'),
-            ),
+            CustomButton(icon: Icons.money, text: 'Fiados', onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => FiadosScreen()),
+              );
+            }),
+            const SizedBox(width: 20),
+            CustomButton(icon: Icons.attach_money, text: 'Recebido', onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const RecebidosScreen()),
+              );
+            }),
+            const SizedBox(width: 20),
+            CustomButton(icon: Icons.edit, text: 'Editar', onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => const EditarScreen()),
+              );
+            }),
           ],
         ),
       ),
@@ -63,37 +57,68 @@ class HomeScreen extends StatelessWidget {
   }
 }
 
-class FiadosScreen extends StatelessWidget {
-  const FiadosScreen({super.key});
+class CustomButton extends StatelessWidget {
+  final IconData icon;
+  final String text;
+  final void Function() onPressed;
+
+  const CustomButton({super.key, required this.icon, required this.text, required this.onPressed});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tela de Fiados'),
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        minimumSize: const Size(20, 20), // Defina o tamanho mínimo aqui
+        maximumSize: const Size(100, 80), // Defina o tamanho máximo aqui
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
-      body: const Center(
-        child: Text('Conteúdo da Tela de Fiados'),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon),
+          const SizedBox(height: 5),
+          Text(text),
+        ],
       ),
     );
   }
 }
 
-class RecebidoScreen extends StatelessWidget {
-  const RecebidoScreen({super.key});
+// class FiadosScreen extends StatelessWidget {
+//   const FiadosScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tela de Recebido'),
-      ),
-      body: const Center(
-        child: Text('Conteúdo da Tela de Recebido'),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Tela de Fiados'),
+//       ),
+//       body: const Center(
+//         child: Text('Conteúdo da Tela de Fiados'),
+//       ),
+//     );
+//   }
+// }
+
+// class RecebidoScreen extends StatelessWidget {
+//   const RecebidoScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Tela de Recebido'),
+//       ),
+//       body: const Center(
+//         child: Text('Conteúdo da Tela de Recebido'),
+//       ),
+//     );
+//   }
+// }
 
 class EditarScreen extends StatelessWidget {
   const EditarScreen({super.key});
