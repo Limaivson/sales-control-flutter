3322fd8c27cfa5686a6b9cd86e09bcc824489b18
Ivson De Lima Santos <124381236+Limaivson@users.noreply.github.com>
2024-04-26 14:50:35 -0300

    feat-adjuste-id

--- sales_control/lib/entities/cliente.dart
+++ sales_control/lib/entities/cliente.dart
@@ -2,10 +2,11 @@ import 'package:sales_control/entities/funcionario.dart';
 import 'package:sales_control/entities/pagamento.dart';
 
 class Cliente{
-  final int id;
+  final String id;
   final String nome;
   final String telefone;
   final String endereco;
+  final String dataCompra;
   late Pagamento pagamento;
   late Funcionario funcionario;
 
@@ -14,6 +15,7 @@ class Cliente{
     required this.nome, 
     required this.telefone, 
     required this.endereco, 
+    required this.dataCompra,
     required this.pagamento, 
     required this.funcionario
   });
