d927e32a7ba42668ee0d93c544c1d38f5ba5042c
Ivson De Lima Santos <124381236+Limaivson@users.noreply.github.com>
2024-04-26 14:58:36 -0300

    refactor-main

--- sales_control/lib/main.dart
+++ sales_control/lib/main.dart
@@ -1,103 +1,15 @@
+// ignore_for_file: depend_on_referenced_packages
 import 'package:flutter/material.dart';
-import 'package:sales_control/page/editar_cliente.dart';
-import 'package:sales_control/page/page_fiados.dart';
-import 'package:sales_control/page/page_recebidos.dart';
+// import 'package:sales_control/page/login.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:sales_control/firebase_options.dart';
+import 'package:sales_control/page/menu_principal.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            CustomButton(
-                icon: Icons.money,
-                text: 'Fiados',
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const FiadosScreen()),
-                  );
-                }),
-            const SizedBox(width: 20),
-            CustomButton(
-                icon: Icons.attach_money,
-                text: 'Recebido',
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const RecebidosScreen()),
-                  );
-                }),
-            const SizedBox(width: 20),
-            CustomButton(
-                icon: Icons.edit,
-                text: 'Editar',
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const EditarScreen()),
-                  );
-                }),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class CustomButton extends StatelessWidget {
-  final IconData icon;
-  final String text;
-  final void Function() onPressed;
-
-  const CustomButton(
-      {super.key,
-      required this.icon,
-      required this.text,
-      required this.onPressed});
-
-  @override
-  Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        minimumSize: const Size(20, 20),
-        maximumSize: const Size(100, 80),
-        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(10),
-        ),
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(icon),
-          const SizedBox(height: 5),
-          Text(text),
-        ],
-      ),
-    );
-  }
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // runApp(const Login());
+  runApp(const Menu());
 }
