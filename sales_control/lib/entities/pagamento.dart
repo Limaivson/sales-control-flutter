f6a7057140204313f8eac87411aa1e7c11ab3028
Ivson De Lima Santos <124381236+Limaivson@users.noreply.github.com>
2024-04-26 18:07:34 -0300

    refactor-this

--- sales_control/lib/entities/pagamento.dart
+++ sales_control/lib/entities/pagamento.dart
@@ -5,16 +5,13 @@ class Pagamento{
   late String tipoPagamento;
   late String dataPagamento;
   late String dataRealizacaoPagamento;
+  final bool pago;
 
   Pagamento({
-    required valor,
-    required tipoPagamento,
-    required dataPagamento,
-    required dataRealizacaoPagamento
-    }){
-    this.valor = valor;
-    this.tipoPagamento = tipoPagamento;
-    this.dataPagamento = dataPagamento;
-    this.dataRealizacaoPagamento = dataRealizacaoPagamento;
-    }
+    required this.valor,
+    required this.tipoPagamento,
+    required this.dataPagamento,
+    required this.dataRealizacaoPagamento,
+    required this.pago
+    });
 }
