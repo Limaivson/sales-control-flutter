597d81bc2416a8f66b03b9cf075889a03c1c40e9
Ivson De Lima Santos <124381236+Limaivson@users.noreply.github.com>
2024-04-27 00:33:27 -0300

    feat-page-recebidos

--- sales_control/lib/page/page_recebidos.dart
+++ sales_control/lib/page/page_recebidos.dart
@@ -1,16 +1,11 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, library_private_types_in_public_api, library_prefixes
+// ignore_for_file: library_private_types_in_public_api, use_super_parameters, library_prefixes
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:sales_control/page/adicionar_cliente.dart';
 import 'package:sales_control/entities/cliente.dart';
+import 'package:sales_control/page/adicionar_cliente.dart';
 import 'package:sales_control/external/obter_clientes.dart' as listaClientes;
 
-void main() {
-  runApp(const MyApp());
-}
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -21,7 +16,7 @@ class MyApp extends StatelessWidget {
 }
 
 class RecebidosScreen extends StatefulWidget {
-  const RecebidosScreen({super.key});
+  const RecebidosScreen({Key? key}) : super(key: key);
 
   @override
   _RecebidosScreenState createState() => _RecebidosScreenState();
@@ -29,55 +24,101 @@ class RecebidosScreen extends StatefulWidget {
 
 class _RecebidosScreenState extends State<RecebidosScreen> {
   late Future<List<Cliente>> clientesFuture;
+  final TextEditingController _valorController = TextEditingController();
+  String _selectedClient = '';
+  late List<Cliente> _clientesFiltrados = [];
 
   @override
   void initState() {
     super.initState();
-    // clientesFuture = listaClientes.ObterListaClientes().obterClientesRecebidos();
+    clientesFuture = listaClientes.ObterListaClientes().obterClientes(false);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Recebidos'),
+        backgroundColor: Colors.blueGrey[900],
+        title: Center(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.5,
+            child: TextField(
+              style: const TextStyle(color: Colors.white),
+              onChanged: (value) {
+                setState(() {
+                  _selectedClient = value;
+                  _filtrarClientes(value); // Não precisamos esperar pelo resultado, pois estamos atualizando a lista localmente
+                });
+              },
+              decoration: const InputDecoration(
+                labelText: 'Buscar por nome',
+                labelStyle: TextStyle(color: Colors.white),
+                prefixIcon: Icon(Icons.search),
+              ),
+            ),
+          ),
+        ),
       ),
-      body: Center(
-        child: FutureBuilder<List<Cliente>>(
-          future: clientesFuture,
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const CircularProgressIndicator();
-            } else if (snapshot.hasError) {
-              return const Text('Erro ao carregar os clientes');
-            } else {
-              List<Cliente> clientes = snapshot.data!;
-              return SingleChildScrollView(
-                child: DataTable(
-                  columns: [
-                    const DataColumn(label: Text('Nome')),
-                    const DataColumn(label: Text('Endereço')),
-                    const DataColumn(label: Text('Telefone')),
-                    const DataColumn(label: Text('Funcionário')),
-                    const DataColumn(label: Text('Pagamento')),
-                    const DataColumn(label: Text('Tipo de Pagamento')),
-                    const DataColumn(label: Text('Data')),
-                  ],
-                  rows: clientes.map((cliente) {
-                    return DataRow(cells: [
-                      DataCell(Text(cliente.nome)),
-                      DataCell(Text(cliente.endereco)),
-                      DataCell(Text(cliente.telefone)),
-                      DataCell(Text(cliente.funcionario.nome)),
-                      DataCell(Text(cliente.pagamento.valor.toString())),
-                      DataCell(Text(cliente.pagamento.tipoPagamento)),
-                      // DataCell(Text(cliente.pagamento.data.toString())),
-                    ]);
+      backgroundColor: Colors.blueGrey[100],
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: FutureBuilder<List<Cliente>>(
+            future: clientesFuture,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return const Text('Erro ao carregar os clientes');
+              } else {
+                List<Cliente> clientes = snapshot.data!;
+                List<Cliente> clientesParaExibir = _selectedClient.isNotEmpty ? _clientesFiltrados : clientes;
+                return ListView(
+                  children: clientesParaExibir.map((cliente) {
+                    return InkWell(
+                      onTap: () {
+                        _mostrarDetalhesCliente(context, cliente);
+                      },
+                      child: Container(
+                        margin: const EdgeInsets.symmetric(vertical: 5.0),
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey[700],
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    cliente.nome,
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                  Text(
+                                    cliente.endereco,
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                ],
+                              ),
+                              IconButton(
+                                icon: const Icon(Icons.attach_money, color: Colors.white),
+                                onPressed: () {
+                                  _mostrarPopupPagamento(context, cliente);
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
                   }).toList(),
-                ),
-              );
-            }
-          },
+                );
+              }
+            },
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
@@ -93,6 +134,97 @@ class _RecebidosScreenState extends State<RecebidosScreen> {
         },
         child: const Icon(Icons.add),
       ),
+    );
+  }
+
+  void _filtrarClientes(String searchTerm) {
+    clientesFuture.then((clientes) {
+      setState(() {
+        _clientesFiltrados = clientes.where((cliente) => cliente.nome.toLowerCase().contains(searchTerm.toLowerCase())).toList();
+      });
+    });
+  }
+
+  Future<void> _mostrarPopupPagamento(BuildContext context, Cliente cliente) async {
+    _valorController.clear();
+
+    showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Registrar Pagamento'),
+          content: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 10),
+                Text('Cliente: ${cliente.nome}'),
+                const SizedBox(height: 20),
+                TextField(
+                  controller: _valorController,
+                  decoration: const InputDecoration(labelText: 'Valor do pagamento'),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancelar'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (_valorController.text.isNotEmpty) {
+                  // Aqui você pode implementar a lógica para registrar o pagamento
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Pagamento registrado com sucesso')),
+                  );
+                  Navigator.of(context).pop();
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Por favor, preencha todos os campos')),
+                  );
+                }
+              },
+              child: const Text('Registrar'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _mostrarDetalhesCliente(BuildContext context, Cliente cliente) {
+    showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(cliente.nome),
+          content: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Endereço: ${cliente.endereco}'),
+                Text('Telefone: ${cliente.telefone}'),
+                Text('Pagamento: ${cliente.pagamento.valor}'),
+                Text('Data de Recebimento: ${cliente.pagamento.dataPagamento}'),
+                Text('Data do Pagamento: ${cliente.pagamento.dataRealizacaoPagamento}'),
+                Text('Funcionário: ${cliente.funcionario.nome}'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Fechar'),
+            ),
+          ],
+        );
+      },
     );
   }
 }
